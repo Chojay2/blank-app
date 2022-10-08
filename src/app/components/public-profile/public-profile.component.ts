45effad93bd3a70c68f341a11184bb6d4a227859
Phuntsho Norbu Tenzin <phuntsho.tenzin@selise.ch>
2022-10-08 14:30:07 +0600

    code refator

--- src/app/components/public-profile/public-profile.component.ts
+++ src/app/components/public-profile/public-profile.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicProfileComponent implements OnInit {
 
+  items: string[];
+  posts = [
+    {
+      cover: '',
+      profile: '',
+      name: '',
+      title: 'This is a blog post title',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
+      date: '2022/10/10',
+      views: '2K Views'
+    },
+    {
+      cover: '',
+      profile: '',
+      name: '',
+      title: 'This is a blog post title',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
+      date: '2022/10/10',
+      views: '2K Views'
+    },
+    {
+      cover: '',
+      profile: '',
+      name: '',
+      title: 'This is a blog post title',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
+      date: '2022/10/10',
+      views: '2K Views'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
