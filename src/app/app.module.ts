b54e0f2b90dbed217a63854d58fa1e521cf0c621
SangayWdee <sangay.wangdi@selise.ch>
2022-10-10 23:52:43 +0600

    feat(login-signup): login-signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,13 @@ import { EndorsementComponent } from './screens/endorsement/endorsement.componen
 import { PostEndorsementComponent } from './screens/post-endorsement/post-endorsement.component';
 import { BlogPostComponent } from './screens/blog-post/blog-post.component';
 import { MapComponent } from './components/map/map.component';
-import { LoginComponent } from './components/auth/login/login.component';
+import { LoginComponent } from './components/auth/auth-container/login/login.component';
 import {TabMenuModule} from "primeng/tabmenu";
 import {ButtonModule} from "primeng/button";
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PublicProfileComponent } from './components/public-profile/public-profile.component';
-import { SignupComponent } from "./components/auth/signup/signup.component";
+import { SignupComponent } from "./components/auth/auth-container/signup/signup.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import { EventComponent } from './screens/event/event.component';
 import {AvatarGroupModule} from "primeng/avatargroup";
@@ -28,6 +28,7 @@ import { HostEventComponent } from './screens/event/host-event/host-event.compon
 import { CalenderComponent } from './screens/calender/calender.component';
 import {GMapModule} from "primeng/gmap";
 import {HttpClientModule} from "@angular/common/http";
+import { AuthContainerComponent } from './components/auth/auth-container/auth-container.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import {HttpClientModule} from "@angular/common/http";
     SignupComponent,
     EventComponent,
     HostEventComponent,
-    CalenderComponent
+    CalenderComponent,
+    AuthContainerComponent
   ],
     imports: [
         BrowserModule,
@@ -61,8 +63,8 @@ import {HttpClientModule} from "@angular/common/http";
         ReactiveFormsModule,
         AvatarGroupModule,
         AvatarModule,
-      HttpClientModule,
-      GMapModule
+        HttpClientModule,
+        GMapModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/auth/srvices/auth.service.ts
+++ src/app/components/auth/srvices/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 // @ts-ignore
-import {LoginFormModel} from "../../models/shared.models";
+import {LoginFormModel, SignUpFormModal} from "../../models/shared.models";
 import {Observable} from "rxjs";
+import {SignUpFromModal} from "../../../models/shared.models";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,9 @@ export class AuthService {
 
   login(body:LoginFormModel): Observable<any> {
    return  this.http.post("https://safe-spire-99671.herokuapp.com/auth/login",body)
+  }
 
+  signup(body:SignUpFromModal): Observable<any> {
+    return this.http.post("https://safe-spire-99671.herokuapp.com/auth/register",body)
   }
 }
