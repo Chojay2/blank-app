8dc6ba8cc978823d1df124fea0babeb9a10daeb9
Chojay02 <chojay.wangchuk@selise.ch>
2022-10-12 12:53:12 +0600

    conflicts

--- src/app/screens/blog-post/blog-post.component.ts
+++ src/app/screens/blog-post/blog-post.component.ts
@@ -10,62 +10,7 @@ import { EndorsementService } from 'src/app/services/endorsement/endorsement.ser
 export class BlogPostComponent implements OnInit {
   posts: any;
   items: string[];
-  // posts = [
-  //   {
-  //     cover: '',
-  //     profile: '',
-  //     name: '',
-  //     title: 'This is a blog post title',
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
-  //     date: '2022/10/10',
-  //     views: '2K Views'
-  //   },
-  //   {
-  //     cover: '',
-  //     profile: '',
-  //     name: '',
-  //     title: 'This is a blog post title',
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
-  //     date: '2022/10/10',
-  //     views: '2K Views'
-  //   },
-  //   {
-  //     cover: '',
-  //     profile: '',
-  //     name: '',
-  //     title: 'This is a blog post title',
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
-  //     date: '2022/10/10',
-  //     views: '2K Views'
-  //   },
-  //   {
-  //     cover: '',
-  //     profile: '',
-  //     name: '',
-  //     title: 'This is a blog post title',
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
-  //     date: '2022/10/10',
-  //     views: '2K Views'
-  //   },
-  //   {
-  //     cover: '',
-  //     profile: '',
-  //     name: '',
-  //     title: 'This is a blog post title',
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
-  //     date: '2022/10/10',
-  //     views: '2K Views'
-  //   },
-  //   {
-  //     cover: '',
-  //     profile: '',
-  //     name: '',
-  //     title: 'This is a blog post title',
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet lorem vebulum, elementum.',
-  //     date: '2022/10/10',
-  //     views: '2K Views'
-  //   },
-  // ];
+  
 
   constructor(private endorsementService: EndorsementService) { 
     this.items = ['All', 'Technology', 'Sports', 'Programming', 'Something', 'Anything'];
