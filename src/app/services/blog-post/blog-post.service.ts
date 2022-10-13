16e511977be10bbeee85c960cb88693a6e833083
chimiWangchukWangdi <chimiwangchukw@gmail.com>
2022-10-13 20:51:21 +0600

    readMore feature in blog-post

---
+++ src/app/services/blog-post/blog-post.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  constructor(private http: HttpClient) { }
+
+  private baseURL = `http://localhost:8080`
+
+
+  getAllPost(): Observable<any> {
+    return this.http.get(`${this.baseURL}/public-posts/get-posts?skip=0&limit=20`)
+ }
+
+ getAPost(id: any): Observable<any> {
+  return this.http.get(`${this.baseURL}/public-posts/get-post?endorsementId=${id}`)
+}
+
+  uploadAPost(data: any): Observable<any> {
+  return this.http.post(`${this.baseURL}/posts/add-post`, data, {headers: new HttpHeaders().set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ0MGM1NjQ4MjVmNDRkODI4Yjk1OWQiLCJ1c2VyVHlwZSI6IkFkbWluIiwiaWF0IjoxNjY1NDk5MTkzfQ.K4XX_II0lJojTOpxypRw0spbg5hgNs5xwfLG7FWqHN0')})
+  }
+
+  updateData(data: any, id: string): Observable<any> {
+    return this.http.post(`${this.baseURL}/posts/edit-post`, data)
+  }
+
+  deleteData(id: any): Observable<any> {
+    return this.http.post(`${this.baseURL}/delete-post`, id)
+}
+
+  addGallery(data: any, file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('title', data.title);
+    formData.append('goal', data.goal);
+    formData.append('description', data.description);
+    const header = new HttpHeaders().set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ0MGM1NjQ4MjVmNDRkODI4Yjk1OWQiLCJ1c2VyVHlwZSI6IkFkbWluIiwiaWF0IjoxNjY1NDg0NjM4fQ.WLCIKyLcnSfm1nyPotqH3VwbBmayyXFJidXsWJYyPc0');
+    const params = new HttpParams();
+
+    const options = {
+      params,
+      reportProgress: true,
+      headers: header
+    };
+
+    const req = new HttpRequest('POST', `${this.baseURL}/posts/add-post`, formData, options);
+    return this.http.request(req);
+  }
+}
